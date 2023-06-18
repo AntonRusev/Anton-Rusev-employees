75d3f3dfbc33a2c60fbc533aa2d710c2af1555f7
AntonRusev <anton_r@abv.bg>
2023-06-18 13:31:55 +0300

    added work overlap checker

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -10,15 +10,19 @@ export const DataProvider = ({
     const [parsedData, setParsedData] = useState(null);
     const [tableRows, setTableRows] = useState(null);
     const [values, setValues] = useState(null);
+    const [workedTogether, setWorkedTogether] = useState(null);
 
     useEffect(() => {
-        console.log('ParsedData STATE:');
-        console.log(parsedData);
-        console.log('ROWS STATE:');
-        console.log(tableRows);
-        console.log('Values STATE:');
-        console.log(values);
-    }, [parsedData, tableRows, values]); 
+        // console.log('ParsedData STATE:');
+        // console.log(parsedData);
+        // console.log('ROWS STATE:');
+        // console.log(tableRows);
+        // console.log('Values STATE:');
+        // console.log(values);
+        if (values) {
+            overlapChecker();
+        }
+    }, [parsedData, tableRows, values]);
 
     const fileHandler = (e) => {
         Papa.parse(e.target.files[0], {
@@ -31,12 +35,12 @@ export const DataProvider = ({
                 results.data.map((d) => {
                     rowsArray.push(Object.keys(d));
                     valuesArray.push(Object.values(d));
-                  });
-                  
+                });
+
                 setParsedData(results.data);
                 setTableRows(rowsArray[0]);
                 setValues(valuesArray);
-                  
+
                 // console.log('Parsed ARRAY:');
                 // console.log(results.data);
                 // console.log('ROWS ARRAY:');
@@ -46,6 +50,44 @@ export const DataProvider = ({
             },
         });
     };
+
+    const overlapChecker = () => {
+        for (let i = 0; i < values.length; i++) {
+            let [empId, projectId, dateFrom, dateTo] = values[i];
+
+            if (dateTo === 'NULL') {
+                dateTo = new Date();
+            };
+
+            const dateOne = new Date(dateFrom);
+            const dateTwo = new Date(dateTo);
+
+            for (let y = i + 1; y < values.length; y++) {
+                let [empIdCompare, projectIdCompare, dateFromCompare, dateToCompare] = values[y];
+
+                if (dateToCompare === 'NULL') {
+                    dateToCompare = new Date();
+                };
+
+                if (projectId === projectIdCompare) {
+
+                    const comparedOne = new Date(dateFromCompare);
+                    const comparedTwo = new Date(dateToCompare);
+
+                    if (comparedTwo >= dateOne || comparedOne <= dateTwo) {
+                        console.log('BINGO')
+                    }
+
+                    console.log(dateOne, dateTwo, comparedOne, comparedTwo);
+                };
+
+            };
+        };
+    };
+
+    const daysCalculator = (employeeOne, employeeTwo, projectId, ) => {
+
+    }
 
     const dataContextValue = {
         fileHandler,
